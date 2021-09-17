e398213a30c3bffef58920a70600e1d8e8a668e3
Alvaro <choco-1997@hotmail.com>
2021-09-17 15:51:26 -0300

    Se crea Orc Mazo y Yelmo Se crean estas clases con sus atributos

---
+++ src/Library/Mazo.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Roleplay_Prog.Library
+{
+    class Mazo
+    {
+        public int Ataque;
+        public int Defensa;
+    }
+}
---
+++ src/Library/Yelmo.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Roleplay_Prog.Library
+{
+    class Yelmo
+    {
+        public int Ataque;
+        public int Defensa;
+    }
+}
