cf21e9f9572e7cc2c76cbfd4106b2aede5da6fdc
[FelipeET] <felipe.etchevers@correo.ucu.edu.uy>
2021-09-17 22:27:28 -0300

    se crea las clases arco armadura y elfo

---
+++ src/Library/Arco.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Roleplay_Prog.Library
+{
+    public class Arco
+
+        {
+            public int Ataque;
+            public int Defensa;
+            
+            public Arco (int ataque,int defensa)
+            {
+                this.Ataque= ataque;
+                this.Defensa= defensa;
+            }
+        }
+
+}  
