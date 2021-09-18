9afcba80563beb38a1a07ab1f2843fcc78c90a88
Juan Riccetto <juanmartinre@gmail.com>
2021-09-18 00:15:28 -0300

    cambiando libro de hechizos

--- src/Library/TunicaCuero.cs
+++ src/Library/TunicaCuero.cs
@@ -9,8 +9,44 @@ namespace Roleplay_Prog.Library
 
         public TunicaCuero(int aAtaque, int aDefensa)
         {
-            Ataque = aAtaque;
-            Defensa = aDefensa;
+            private int ataque;
+        public int Ataque
+        {
+            get
+            {
+                return this.Ataque;
+            }
+            set
+            {
+                if(value >=0)
+                {
+                    this.Ataque = value;
+                }
+                else
+                {
+                    this.ataque = 0;
+                }
+            }
+        }
+        private int defensa;
+        public int Defensa
+        {
+            get
+            {
+                return this.Defensa;
+            }
+            set
+            {
+                if(value >=0)
+                {
+                    this.Defensa = value;
+                }
+                else
+                {
+                    this.defensa = 0;
+                } 
+            }
+        }
         }
     }
 }
